f185487c1a520127a22cb75721705042600a0102
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2025-12-20 20:47:55 +0100

    Add interests page

--- src/app/interests/page.tsx
+++ src/app/interests/page.tsx
@@ -1,7 +1,23 @@
+import Waf from "@/content/interests/interest_waf.mdx"
+import Photography from "@/content/interests/interest_photography.mdx"
+
 export default function InterestsPage() {
   return (
-    <div className="grid grid-cols-2 h-full">
-      Hello World
+    <div className="grid grid-cols-1 md:grid-cols-2 h-full">
+      <div className="border-r p-6 border-neutral-300">
+        <Photography/>
+      </div>
+
+      {/* Interesse piccolo 2 */}
+      <div className="rounded-xl border p-6">
+ 
+      </div>
+
+      {/* Interesse grande */}
+      <div className="border-t border-neutral-300 p-6 md:col-span-2">
+        <Waf/>
+      </div>
+
     </div>
   );
 }
