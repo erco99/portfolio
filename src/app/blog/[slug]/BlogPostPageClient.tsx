062ec136a7b3330ef77f7bc289eaae9825d943e3
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2026-01-18 14:33:38 +0100

    Remove problematic toc configuration, add temporary one

--- src/app/blog/[slug]/BlogPostPageClient.tsx
+++ src/app/blog/[slug]/BlogPostPageClient.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useLayoutEffect, useRef, useState } from "react";
 
 type Props = {
   mdxpost: React.ReactNode;
@@ -7,78 +6,14 @@ type Props = {
 };
 
 export default function BlogPostWithAnchoredTOC({ mdxpost, toc }: Props) {
-  const tocRef = useRef<HTMLDivElement>(null);
-  const placeholderRef = useRef<HTMLDivElement>(null);
-  const [isFixed, setIsFixed] = useState(false);
-  const distanceFromTop = 50;
-
-  const [showTOC, setShowTOC] = useState(true);
-  const [isHiding, setIsHiding] = useState(false);
-
-  const hideTOC = () => {
-    setIsHiding(true);
-    setTimeout(() => {
-      setShowTOC(false);
-      setIsHiding(false);
-    }, 300);
-  };
-
-  useLayoutEffect(() => {
-    const handleScroll = () => {
-      if (!placeholderRef.current) return;
-      const top = placeholderRef.current.getBoundingClientRect().top;
-      setIsFixed(top <= distanceFromTop);
-    };
-
-    handleScroll();
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
-    <div
-      className={`
-        grid gap-8 h-full
-        ${showTOC ? "grid-cols-1 lg:grid-cols-[1fr_250px]" : "grid-cols-1 pl-30 pr-30"}
-      `}
-    >
-      <div className="p-4 prose dark:prose-invert">
+    <div className="relative flex justify-center">
+      <aside className="hidden xl:block fixed right-8 top-32 w-64">
+        {toc}
+      </aside>
+      <article className="prose dark:prose-invert max-w-3xl">
         {mdxpost}
-      </div>
-
-      {showTOC && (
-        <div className="hidden lg:block border-l border-neutral-300 relative">
-          <div
-            ref={placeholderRef}
-            className="w-52"
-            style={{ height: tocRef.current?.offsetHeight }}
-          >
-            <div
-              ref={tocRef}
-              className={`p-4 bg-white dark:bg-neutral-900 transition-all
-                ${isFixed ? "fixed top-[50px] w-52 z-50" : "relative"}
-              `}
-            >
-              {toc}
-            </div>
-          </div>
-        </div>
-      )}
-      <button
-        onClick={() => {
-          if (showTOC) hideTOC();
-          else setShowTOC(true);
-        }}
-        className={`
-          fixed top-1/2 transform -translate-y-1/2 z-50 
-          rounded-md bg-neutral-200 dark:bg-neutral-800
-          px-3 py-1 text-lg
-          hover:bg-neutral-300 dark:hover:bg-neutral-700 cursor-pointer
-          right-0
-        `}
-      >
-        {showTOC ? ">" : "<"}
-      </button>
+      </article>
     </div>
   );
 }
