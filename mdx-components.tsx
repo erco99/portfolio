4204ad4a73a2f66741e7b197ceadca322d18a8d1
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2026-01-24 15:06:07 +0100

    Fix bad toc

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -117,29 +117,29 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       </pre>
     ),
 
-img: ({ alt, ...props }) => (
-  <figure className="my-6">
-    <img
-      className="rounded-lg border border-neutral-300 mx-auto"
-      alt={alt}
-      {...props}
-    />
-    {alt && (
-      <figcaption className="mt-2 text-sm text-center text-neutral-600 dark:text-neutral-400">
-        {alt}
-      </figcaption>
-    )}
-  </figure>
-),
+    img: ({ alt, ...props }) => (
+      <figure className="my-6">
+        <img
+          className="rounded-lg border border-neutral-300 mx-auto"
+          alt={alt}
+          {...props}
+        />
+        {alt && (
+          <figcaption className="mt-2 text-sm text-center text-neutral-600 dark:text-neutral-400">
+            {alt}
+          </figcaption>
+        )}
+      </figure>
+    ),
 
-table: (props) => (
-  <div className="overflow-x-auto my-6">
-    <table
-      className="w-full border border-gray-300 border-collapse text-sm rounded-lg"
-      {...props}
-    />
-  </div>
-),
+    table: (props) => (
+      <div className="overflow-x-auto my-6">
+        <table
+          className="w-full border border-gray-300 border-collapse text-sm rounded-lg"
+          {...props}
+        />
+      </div>
+    ),
 
     thead: (props) => (
       <thead
--- src/app/blog/[slug]/BlogPostPageClient.tsx
+++ src/app/blog/[slug]/BlogPostPageClient.tsx
@@ -8,12 +8,20 @@ type Props = {
 export default function BlogPostWithAnchoredTOC({ mdxpost, toc }: Props) {
   return (
     <div className="relative flex justify-center">
-      <aside className="hidden xl:block fixed right-8 top-32 w-64">
-        {toc}
-      </aside>
       <article className="prose dark:prose-invert max-w-3xl">
         {mdxpost}
       </article>
+
+      <aside
+        className="hidden xl:block fixed top-1/2 -translate-y-1/2 w-64"
+        style={{
+          left: `calc(50% + 384px + 250px)`,
+        }}
+      >
+        <div className="bg-white dark:bg-neutral-900 border border-gray-300 border-neutral-300 shadow-lg p-4 max-h-[50vh] overflow-y-auto">
+          {toc}
+        </div>
+      </aside>
     </div>
   );
 }
