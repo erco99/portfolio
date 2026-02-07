cf4a2433cbcb2f83a4c338bead57b4350b9ca171
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2026-02-07 12:02:43 +0100

    Change page content: height is fixed and scrolling is enable under a certain height

--- src/components/page-content.tsx
+++ src/components/page-content.tsx
@@ -4,14 +4,13 @@ import { Header } from "@/components/header";
 interface PageContentProps {
   children: React.ReactNode;
 }
-
 export function PageContent({ children }: PageContentProps) {
   return (
-    <div className="min-h-screen flex justify-center bg-zinc-50 font-sans dark:bg-black p-4">
-      <div className="w-full max-w-6xl bg-white dark:bg-neutral-950 border border-neutral-200 shadow overflow-hidden flex flex-col">
-        <div className="p-6 sm:p-8 flex flex-col flex-1">
+    <div className="h-screen flex items-center justify-center bg-zinc-50 font-sans dark:bg-black p-4">
+      <div className="w-full max-w-6xl h-full max-h-[1200px] bg-white dark:bg-neutral-950 border border-neutral-200 shadow flex flex-col">
+        <div className="flex flex-col h-full p-6 sm:p-8">
           <Header />
-          <div className="flex-1 overflow-auto border border-neutral-300 bg-neutral-100 dark:bg-neutral-900 text-neutral-800 dark:text-erco-color">
+          <div className="flex-1 overflow-y-auto border border-neutral-300 bg-neutral-100 dark:bg-neutral-900 text-neutral-800 dark:text-erco-color">
             {children}
           </div>
         </div>
