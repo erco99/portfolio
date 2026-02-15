eed6a77faa4fcaaeedd0cb0a7794a125d4a16559
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2026-02-15 10:40:15 +0100

    Add mobile version of header

--- src/components/page-content.tsx
+++ src/components/page-content.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Header } from "@/components/header";
+import { Header } from "@/components/header/header";
 
 interface PageContentProps {
   children: React.ReactNode;
